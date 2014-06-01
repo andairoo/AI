537d7b6c5b37efa1f0ee5e65157414be02733ecf
andairoo <andrew.dai@hotmail.com>
2014-06-01 12:52:08 +1000

    structure

--- mrPass.c
+++ mrPass.c
@@ -15,10 +15,19 @@
  
 action decideAction (Game g) {
  
-   
-   action nextAction;
-   nextAction.actionCode = PASS;
-   
+    if (currentPlayer = 1) {
+        
+        if (STUDENT_THD < 1 && STUDENT_MTV < 1 && STUDENT_MMONEY < 1) {
+    
+        action nextAction;
+        nextAction.actionCode = PASS;
+        }
+       
+        else {
+        // enter spinoff logic here i.e. 1/3 chance for IP, 2/3 chance for publication
+        }
+       
+    }
    return nextAction;
 }
 
